b3691c3fd0f05afa795436a9757ecfbb33ce235b
osu28 <osu@purdue.edu>
2023-05-14 15:27:08 -0400

    created components

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+// PostList.js
+
+import React from 'react';
+
+const PostList = () => {
+  return (
+    <div>
+      {/* This will display the list of posts */}
+    </div>
+  );
+};
+
+export default PostList;
