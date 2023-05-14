66cdd287bf1c463f3534f344ce51175681f762a3
osu28 <osu@purdue.edu>
2023-05-14 16:46:05 -0400

    ready for backend

---
+++ src/components/Post.js
@@ -0,0 +1,13 @@
+// PostList.js
+
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div className='Article'>
+      <p>My article!!</p>
+    </div>
+  );
+};
+
+export default Post;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,12 @@
 // PostList.js
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PostList = () => {
   return (
     <div className='List_Entry'>
-      <p><a href="/post-link">Title of the Post</a></p>
+      <p><Link to="/post-link">Title of the Post</Link></p> {/* Use Link instead of a */}
       <p>May 14, 2023</p>
     </div>
   );
