8502b773cad6ec1bb8fa0d8b21f979f82a0bb3cb
osu28 <osu@purdue.edu>
2023-05-15 18:01:28 -0400

    npm build

---
+++ blog-frontend/build/precache-manifest.40f8ceaaf24a9205fb730dfffbad38a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf2a817ae69c27b0b01e",
+    "url": "/static/css/main.5dedb55b.chunk.css"
+  },
+  {
+    "revision": "cf2a817ae69c27b0b01e",
+    "url": "/static/js/main.58fe6755.chunk.js"
+  },
+  {
+    "revision": "44379f9d8a742a556af9",
+    "url": "/static/js/runtime~main.1b973b83.js"
+  },
+  {
+    "revision": "61e941aa1a127c283727",
+    "url": "/static/js/2.fe8ebadd.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "aa518748fbbbcadd46d34970933000f5",
+    "url": "/index.html"
+  }
+];
