d21a90b72edd35b0982ca1b77b09b0b3a7c09eba
osu28 <osu@purdue.edu>
2023-05-16 13:25:27 -0400

    configured database to use mondodb atlas

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const articlesRouter = require('./routes/articles');
 const Article = require('./models/article');
 
-mongoose.connect('mongodb://localhost/my-blog', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+db.once('open', () => console.log('Connected to MongoDB Atlas'));
 
 const app = express();
 
@@ -16,51 +17,36 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to My Blog API' });
-});  
+  res.json({ message: 'Welcome to My Blog API' });
+});
 
 // Route to fetch all articles
 app.get('/articles', async (req, res) => {
-    try {
-      // Fetch articles from the database
-      const articles = await Article.find();
-  
-      // Send the articles as a response
-      res.json(articles);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+  try {
+    // Fetch articles from the database
+    const articles = await Article.find();
+
+    // Send the articles as a response
+    res.json(articles);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-// app.get('/articles/:id', async (req, res) => {
-//     const articleId = req.params.id;
-  
-//     try {
-//       const article = await Article.findById(articleId);
-//       if (!article) {
-//         return res.status(404).json({ message: 'Article not found' });
-//       }
-//       res.json(article);
-//     } catch (error) {
-//       res.status(500).json({ message: 'Server error' });
-//     }
-// });
-
+// Route to fetch an article by slug
 app.get('/articles/:slug', async (req, res) => {
-    const slug = req.params.slug;
-  
-    try {
-      const article = await Article.findOne({ slug });
-      if (!article) {
-        return res.status(404).json({ message: 'Article not found' });
-      }
-      res.json(article);
-    } catch (error) {
-      res.status(500).json({ message: 'Server error' });
+  const slug = req.params.slug;
+
+  try {
+    const article = await Article.findOne({ slug });
+    if (!article) {
+      return res.status(404).json({ message: 'Article not found' });
     }
+    res.json(article);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 });
-  
-  
 
 app.use('/articles', articlesRouter);
 
